3509d42143050ebce5359c008daf2448834aad52
Woomymy <github@woomy.ovh>
2021-03-21 10:07:51 +0100

    EA6350V3: Fix setup script

--- EA6350V3/router.sh
+++ EA6350V3/router.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 set -e
-PACKAGES=(luci-lib-ipkg luci-compat relayd luci-proto-relay luci-app-unbound luci-app-wol luci-app-advanced-reboot libustream-openssl)
+PACKAGES="luci-lib-ipkg luci-compat relayd luci-proto-relay luci-app-unbound luci-app-wol luci-app-advanced-reboot libustream-openssl"
 install_packages() {
 	echo -e "\e[96mInstalling packages...\e[m"
 	opkg update
-	for PACK in "${PACKAGES[*]}"
+	for PACK in ${PACKAGES}
 	do
 		opkg install "${PACK}"
 	done
 }
 install_themes() {
 	echo -e "\e[96mSetting up themes...\e[m"
-	URLS=(https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.2.5/luci-theme-argon_2.2.5-20200914_all.ipk https://github.com/jerrykuku/luci-app-argon-config/releases/download/v0.8-beta/luci-app-argon-config_0.8-beta_all.ipk)
-	for URL in "${URLS[*]}"
+	URLS="https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.2.5/luci-theme-argon_2.2.5-20200914_all.ipk https://github.com/jerrykuku/luci-app-argon-config/releases/download/v0.8-beta/luci-app-argon-config_0.8-beta_all.ipk"
+	for URL in ${URLS}
 	do
 		if [[ -f "temp.ipk" ]]
 		then
